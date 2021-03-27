0f71df6d4ad06306df6e3309a9e64668f2b26952
Trey Lowerison <19714082+tlowerison@users.noreply.github.com>
2021-03-27 01:52:52 -0700

    add signin command, useful in docker builds

--- main.go
+++ main.go
@@ -27,12 +27,9 @@ var mode = Mode(ModeLdFlag)
 
 var app = fmt.Sprintf("%s-credential-1password", string(mode))
 
-// util
-
 var homeDir string
-const whitespace = " \n\t"
 
-// config
+// --- config ---
 
 var configPath = ""
 const timeFormat = time.UnixDate
@@ -55,7 +52,7 @@ var vaultUUIDKey = fmt.Sprintf("%s.uuid", vaultKey)
 var vaultDescription = fmt.Sprintf("Contains credentials managed by %s.", app)
 var missinVaultErrMsg = "no vault found with name: \"%s\"\ncreate a new vault with: %s vault <vault-name>"
 
-// inputs
+// --- stdin inputs ---
 
 var (
   key      string
@@ -64,45 +61,50 @@ var (
   URL      *url.URL
 )
 
-// main
-
 func main() {
   RegisterConfig()
 
   rootCmd := &cobra.Command{
     Use:   app,
-    Short: fmt.Sprintf("credential helper for 1Password.", app),
+    Short: "credential helper for 1Password",
     Run: func(cmd *cobra.Command, _ []string) {
       fmt.Println(cmd.UsageString())
     },
   }
 
   rootCmd.AddCommand(&cobra.Command{
+    Use:   "signin",
+    Short: "signin through stdin to create 1password session token",
+    Args:  cobra.ExactArgs(0),
+    Run:   asRun(Signin),
+  })
+
+  rootCmd.AddCommand(&cobra.Command{
     Use:   "vault",
     Short: "get/set the vault that credential uses",
-    Args: cobra.RangeArgs(0, 1),
-    Run: Vault,
+    Args:  cobra.RangeArgs(0, 1),
+    Run:   asRunWithArgs(Vault),
   })
 
   rootCmd.AddCommand(&cobra.Command{
-    Use:   "get",
-    Short: "get credential by key",
+    Use:    "get",
+    Short:  "get credential by key",
     PreRun: PreRun,
-    Run: Get,
+    Run:   asRun(Get),
   })
 
   rootCmd.AddCommand(&cobra.Command{
-    Use:   "store",
-    Short: "store key=value pair",
+    Use:    "store",
+    Short:  "store key=value pair",
     PreRun: PreRun,
-    Run: Store,
+    Run:    asRun(Store),
   })
 
   rootCmd.AddCommand(&cobra.Command{
-    Use:   "erase",
-    Short: "erase credential by key",
+    Use:    "erase",
+    Short:  "erase credential by key",
     PreRun: PreRun,
-    Run: Erase,
+    Run:    asRun(Erase),
   })
 
   rootCmd.Execute()
@@ -129,59 +131,97 @@ func RegisterConfig() {
   }
 }
 
-// pre-run
+// --- command pre-runs ---
 
 func PreRun(cmd *cobra.Command, _ []string) {
-  ParseURL(cmd)
-  PreRunSessionToken()
+  handleErr(ParseURL(cmd))
+  handleErr(PreRunSessionToken())
+
   name := viper.Get(vaultNameKey)
   shouldUpsertVault := (name == nil || name == "")
   if shouldUpsertVault {
     viper.Set(vaultNameKey, defaultVaultName)
   }
-  PreRunVaultUUID(shouldUpsertVault)
+
+  handleErr(PreRunVaultUUID(shouldUpsertVault))
 }
 
-func PreRunSessionToken() {
+func PreRunSessionToken() error {
   token, err := GetSessionToken()
   if err != nil {
-    os.Exit(1)
+    return err
   }
 
   if token == "" {
     token, err = CreateSessionToken()
   }
   if err != nil {
-    os.Exit(1)
+    return err
   }
 
   sessionToken = token
   viper.Set(sessionTokenDateKey, time.Now().Format(timeFormat))
   viper.Set(sessionTokenValueKey, sessionToken)
   viper.WriteConfig()
+
+  return nil
 }
 
-func PreRunVaultUUID(shouldUpsert bool) {
+func PreRunVaultUUID(shouldUpsert bool) error {
   vaultName = GetVaultName()
   uuid, err := GetVaultUUID(shouldUpsert)
-  if (!shouldUpsert && uuid == "") || err != nil {
-    os.Exit(1)
+  if err != nil {
+    return err
   }
+  if (!shouldUpsert && uuid == "") {
+    return fmt.Errorf("unable to get specified vault's uuid")
+  }
+
   if shouldUpsert && uuid == "" {
     uuid, err = UpsertVaultUUID()
   }
+  if err != nil {
+    return err
+  }
   if uuid == "" {
-    os.Exit(1)
+    return fmt.Errorf("unable to get specified vault's uuid")
   }
 
   vaultUUID = uuid
   viper.Set(vaultUUIDKey, vaultUUID)
   viper.WriteConfig()
+
+  return nil
 }
 
-// command
+// --- command wrappers ---
 
-func Vault(_ *cobra.Command, args []string) {
+func asRun(fn func() error) func(_ *cobra.Command, _ []string) {
+  return func(_ *cobra.Command, _ []string) {
+    handleErr(fn())
+  }
+}
+
+func asRunWithArgs(fn func(args []string) error) func(_ *cobra.Command, args []string) {
+  return func(_ *cobra.Command, args []string) {
+    handleErr(fn(args))
+  }
+}
+
+func handleErr(err error) {
+  if err != nil {
+    fmt.Fprintln(os.Stderr, err.Error())
+    os.Exit(1)
+  }
+}
+
+// --- commands ---
+
+func Signin() error {
+  return PreRunSessionToken()
+}
+
+func Vault(args []string) error {
   if len(args) == 0 {
     fmt.Println(GetVaultName())
   } else {
@@ -195,16 +235,16 @@ func Vault(_ *cobra.Command, args []string) {
     PreRunVaultUUID(true)
     viper.WriteConfig()
   }
+  return nil
 }
 
 // Get retrieves a credential from 1Password
-func Get(_ *cobra.Command, _ []string) {
-  bytes, err := OpGet(false, "item", key, "--vault", vaultUUID)
+func Get() error {
+  outBytes, err := OpGet(false, "item", key, "--vault", vaultUUID)
   if err != nil {
-    println(err.Error())
-    os.Exit(1)
+    return err
   }
-  output := string(bytes)
+  output := string(outBytes)
 
   queryField := func(field string) string {
     query := fmt.Sprintf("details.fields.#(designation==\"%s\").value", field)
@@ -241,14 +281,15 @@ func Get(_ *cobra.Command, _ []string) {
   default:
     break
   }
+  return nil
 }
 
 // Store upserts a credential in 1Password
-func Store(_ *cobra.Command, _ []string) {
+func Store() error {
   // value := fmt.Sprintf("%s:%s", username, password)
   item, err := OpGet(false, "item", key, "--vault", vaultUUID)
   if err != nil {
-    os.Exit(1)
+    return err
   }
 
   uuid := gjson.Get(item, "uuid").String()
@@ -256,7 +297,7 @@ func Store(_ *cobra.Command, _ []string) {
   storedPassword := gjson.Get(item, "password").String()
 
   if username == storedUsername && password == storedPassword {
-    os.Exit(1)
+    return nil
   }
 
   opArgs := []string{fmt.Sprintf("title=%s", key), fmt.Sprintf("username=%s", username), fmt.Sprintf("password=%s", password), "--vault", vaultUUID}
@@ -266,31 +307,27 @@ func Store(_ *cobra.Command, _ []string) {
   } else {
     _, err = Op(append([]string{"edit", "item", uuid}, opArgs...)...)
   }
-  if err != nil {
-    os.Exit(1)
-  }
+  return err
 }
 
 // Erase removes a credential in 1Password
-func Erase(_ *cobra.Command, _ []string) {
+func Erase() error {
   item, err := OpGet(false, "item", key, "--vault", vaultUUID)
   if err != nil {
-    os.Exit(1)
+    return err
   }
 
   uuid := gjson.Get(item, "uuid").String()
 
   if item == "" {
-    os.Exit(1)
+    return fmt.Errorf("unable to find item")
   }
 
   _, err = Op("delete", "item", uuid, "--vault", vaultUUID)
-  if err != nil {
-    os.Exit(1)
-  }
+  return err
 }
 
-// cached value helpers
+// --- cached value helpers ---
 
 func GetVaultName() string {
   vaultNameIntf := viper.Get(vaultNameKey)
@@ -310,18 +347,18 @@ func GetVaultUUID(silent bool) (string, error) {
     }
   }
 
-  bytes, err := OpGet(silent, "vault", vaultName)
+  outBytes, err := OpGet(silent, "vault", vaultName)
   if err != nil {
     return "", err
   }
 
-  return gjson.Get(string(bytes), "uuid").String(), nil
+  return gjson.Get(string(outBytes), "uuid").String(), nil
 }
 
 // UpsertVaultUUID calls creates a new 1Passord vault and returns
 // the newly created vault's uuid on success.
 func UpsertVaultUUID() (string, error) {
-  bytes, err := Op(
+  outBytes, err := Op(
     "create",
     "vault", vaultName,
     "--allow-admins-to-manage", "false",
@@ -331,7 +368,7 @@ func UpsertVaultUUID() (string, error) {
     return "", err
   }
 
-  uuid := gjson.Get(string(bytes), "uuid").String()
+  uuid := gjson.Get(string(outBytes), "uuid").String()
   return uuid, nil
 }
 
@@ -355,16 +392,15 @@ func GetSessionToken() (string, error) {
 // CreateSessionToken requests the user to sign into 1Password through
 // stdin, then stores the provided session token in the local config.
 func CreateSessionToken() (string, error) {
-  bytes, err := Op("signin", "--raw")
+  outBytes, err := Op("signin", "--raw")
   if err != nil {
-    println()
     return "", err
   }
 
-  return strings.Trim(string(bytes), whitespace), nil
+  return strings.TrimSpace(string(outBytes)), nil
 }
 
-// op helpers
+// --- op helpers ---
 
 // Op wraps 1Password's cli tool op.
 func Op(args ...string) ([]byte, error) {
@@ -379,14 +415,15 @@ func OpGet(silent bool, args ...string) (string, error) {
   cmd := exec.Command("op", append(append([]string{"get"}, args...), "--session", sessionToken)...)
   cmd.Stdin = os.Stdin
 
-  bytes, _ := cmd.CombinedOutput()
-  out := strings.Trim(string(bytes), whitespace)
+  outBytes, _ := cmd.CombinedOutput()
+  out := strings.TrimSpace(string(outBytes))
 
   if !silent && strings.Contains(out, "doesn't seem to be a vault in this account") {
-    println(fmt.Sprintf(missinVaultErrMsg, vaultName, app))
+    err := fmt.Errorf(missinVaultErrMsg, vaultName, app)
+    fmt.Fprintln(os.Stderr, err.Error())
     viper.Set(vaultUUIDKey, "")
     viper.WriteConfig()
-    os.Exit(1)
+    return "", err
   }
   if strings.HasPrefix(out, "[ERROR]") {
     return "", nil
@@ -394,10 +431,10 @@ func OpGet(silent bool, args ...string) (string, error) {
   return out, nil
 }
 
-// helpers
+// --- helpers ---
 
 // ParseURL reads from stdin and sets key, username and password.
-func ParseURL(cmd *cobra.Command) {
+func ParseURL(cmd *cobra.Command) error {
   URL = &url.URL{}
   var rawurl string
 
@@ -423,9 +460,9 @@ func ParseURL(cmd *cobra.Command) {
   if mode == dockerMode {
     input := strings.Join(lines, "\n")
     if cmd.Use == "store" {
-      rawurl = strings.TrimPrefix(gjson.Get(input, "ServerURL").String(), whitespace)
-      username = strings.TrimPrefix(gjson.Get(input, "Username").String(), whitespace)
-      password = strings.TrimPrefix(gjson.Get(input, "Secret").String(), whitespace)
+      rawurl = strings.TrimSpace(gjson.Get(input, "ServerURL").String())
+      username = strings.TrimSpace(gjson.Get(input, "Username").String())
+      password = strings.TrimSpace(gjson.Get(input, "Secret").String())
     } else {
       rawurl = input
     }
@@ -435,7 +472,7 @@ func ParseURL(cmd *cobra.Command) {
     var err error
     URL, err = url.Parse(rawurl)
     if err != nil {
-      os.Exit(1)
+      return err
     }
     if URL.User.Username() != "" {
       username = URL.User.Username()
@@ -444,4 +481,5 @@ func ParseURL(cmd *cobra.Command) {
   }
 
   key = URL.String()
+  return nil
 }
